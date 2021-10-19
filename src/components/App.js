0dcfac3dfbafa80ca8ef982788e5ef7d8498fa38
olehhapuk <oleggapuk20@gmail.com>
2021-10-19 19:29:04 +0300

    Finished collections

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,38 @@
 import VideoCard from './VideoCard';
 import Header from './Header';
+import videosData from '../videos.json';
+
+console.log(videosData);
 
 function App() {
   return (
     <>
       <Header filmCount={2}></Header>
 
-      <VideoCard
+      {/* <VideoCard
         imageUrl="https://image.tmdb.org/t/p/w500/xmbU4JTUm8rsdtn7Y3Fcm30GpeT.jpg"
         title="Free Guy"
         description="A bank teller called Guy realizes he is a background character in an open world video game called Free City that will soon go offline."
         isFavorite={false}
-      />
-      <VideoCard
-        imageUrl="https://image.tmdb.org/t/p/w500/3cUj0JRFcXmN7G2vBuWbRUQfjhw.jpg"
-        title="Fever Dream"
-        description="A woman named Amanda lies stricken, far from home. A young boy named David questions her, trying to make her remember. She's not his mother, he's not her son. As her time is running out, he helps her unravel a powerful, haunting story of obsessive jealousy, an invisible danger, and the power of a mother's love for her child."
-        isFavorite={true}
-      />
+      /> */}
+      {videosData.map((video) => (
+        <VideoCard
+          key={video.id}
+          // key={1} Не унікальний
+          // key={new Date().toDateString()} Не стабільний
+          imageUrl={video.poster_path}
+          title={video.title}
+          description={video.overview}
+          isFavorite={false}
+          // {...video}
+        />
+      ))}
+
+      {/* {videosData.map((video) => (
+        <div key={}>
+          <p>{video.title}</p>
+        </div>
+      ))} */}
     </>
   );
 }
